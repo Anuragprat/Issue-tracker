bb867c32c6a4c83a7253affb287a5fd25c7346ff
Anurag <anuragtivari850@gmail.com>
2024-06-27 16:01:38 +0530

    added Link Styles

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { GiAlienBug } from "react-icons/gi";
+import { text } from "stream/consumers";
+
 const NavBar = () => {
+  const path = usePathname();
+  console.log(path);
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -15,7 +23,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-900 hover:text-zinc-500 transition-colors"
+            className={classNames({
+              "text-zinc-950": path === link.href,
+              "text-zinc-500": path !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
